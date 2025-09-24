c9077b3d08ee1e28a9497761ef1326a102815421
Imane Benammou <imanebenamou@gmail.com>
2025-09-24 23:01:06 +0100

    Delete application directory

--- application/orange_pest_detector.py
+++
@@ -1,9 +0,0 @@
-# This Python file uses the following encoding: utf-8
-from PyQt6 import QtCore
-from PyQt6 import QtWidgets
-from PyQt6 import QtQuick
-
-
-class orange_pest_detector(QtQuick.QQuickItem):
-    def __init__(self):
-        pass
